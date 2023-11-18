5a1b1f32463c04184f0b826fc96a9e78057a1e3c
deblur99 <deblur99@kakao.com>
2023-11-18 15:19:07 +0900

    Initial Commit

---
+++ BLESampleApp Watch App/BLESampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLESampleAppApp.swift
+//  BLESampleApp Watch App
+//
+//  Created by 한현민 on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BLESampleApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLESampleApp/BLESampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLESampleAppApp.swift
+//  BLESampleApp
+//
+//  Created by 한현민 on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BLESampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
